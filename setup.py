df98648d7d1e8d81d5028fdb3337b8253eb3e245
Enzo Busseti <enzo.busseti@me.com>
2019-05-29 00:42:13 +0200

    bug fix

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='tsar',
-    version='0.0.3',
+    version='0.0.4',
     description='Time series auto-regressor.',
     author='Enzo Busseti',
+    license='GPLv3+',
     packages=['tsar'],
     install_requires=['numpy', 'pandas', 'numba']
 )
