2e7e563071994750b094107c050d0864940aa486
Enzo Busseti <enzo.busseti@me.com>
2019-11-21 08:17:40 +0100

    0.8.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tsar',
-    version='0.8.0',
+    version='0.8.1',
     description='Time series auto-regressor.',
     author='Enzo Busseti',
     license='GPLv3+',
